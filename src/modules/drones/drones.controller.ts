62bab05b9d15c1134086db7249c04c646bb182d2
Norbert Agu <agunorbert@gmail.com>
2025-11-14 10:21:42 +0100

    feat: add REST API endpoints for drone operations

---
+++ src/modules/drones/drones.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { DronesService } from './drones.service';
+import { CreateDroneDto } from './dto/create-drone.dto';
+import { LoadMedicationDto } from './dto/load-medication.dto';
+
+@Controller('api/drones')
+export class DronesController {
+  constructor(private readonly dronesService: DronesService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createDroneDto: CreateDroneDto) {
+    return this.dronesService.create(createDroneDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.dronesService.findAll();
+  }
+
+  @Get('available')
+  findAvailable() {
+    return this.dronesService.findAvailable();
+  }
+
+  @Get(':serialNumber')
+  findOne(@Param('serialNumber') serialNumber: string) {
+    return this.dronesService.findOne(serialNumber);
+  }
+
+  @Post(':serialNumber/load')
+  @HttpCode(HttpStatus.OK)
+  async loadMedications(
+    @Param('serialNumber') serialNumber: string,
+    @Body() loadMedicationDto: LoadMedicationDto,
+  ) {
+    const drone = await this.dronesService.loadMedications(
+      serialNumber,
+      loadMedicationDto,
+    );
+
+    return {
+      message: 'Drone loaded successfully',
+      drone,
+    };
+  }
+
+  @Get(':serialNumber/medications')
+  getMedications(@Param('serialNumber') serialNumber: string) {
+    return this.dronesService.getMedications(serialNumber);
+  }
+
+  @Get(':serialNumber/battery')
+  getBatteryLevel(@Param('serialNumber') serialNumber: string) {
+    return this.dronesService.getBatteryLevel(serialNumber);
+  }
+}
--- src/modules/drones/drones.module.ts
+++ src/modules/drones/drones.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DronesController } from './drones.controller';
 import { DronesService } from './drones.service';
 import { Drone } from './entities/drone.entity';
 import { Medication } from '../medications/entities/medication.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Drone, Medication])],
+  controllers: [DronesController],
   providers: [DronesService],
   exports: [DronesService],
 })
